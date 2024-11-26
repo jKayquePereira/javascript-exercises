9327efdec6bd09d1e79918ab7951d8b165c9a3b6
Kayque Pereira <114672278+jKayquePereira@users.noreply.github.com>
2024-11-26 20:02:49 -0300

    exc8 finalizado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(numAdd1, numAdd2) {
+  return numAdd1 + numAdd2
 };
 
-const subtract = function() {
-	
+const subtract = function(numSub1, numSub2) {
+  return numSub1 - numSub2
 };
 
-const sum = function() {
-	
+const sum = function(...numSum) {
+  if (numSum.length === 1 && Array.isArray(numSum[0])) {
+    numSum = numSum[0];
+  }
+
+  let sumAll = 0
+
+  for (i = 0; i < numSum.length; i++) {
+    sumAll += numSum[i]
+  }
+
+  return Number(sumAll)
 };
 
-const multiply = function() {
+const multiply = function(...numMult) {
+    if (numMult.length === 1 && Array.isArray(numMult[0])) {
+      numMult = numMult[0];
+    }
 
+    let MultAll = 1;
+
+    for (let i = 0; i < numMult.length; i++) {
+      MultAll *= numMult[i];
+    }
+
+    return MultAll;
+  };
+
+
+const power = function(numPower1, numPower2) {
+  return numPower1 ** numPower2
 };
 
-const power = function() {
-	
+const factorial = function(numFac) {
+  let result = 1
+
+  for (i = numFac; i > 0; i--) {
+    result *= i
+  }
+  return result
 };
 
-const factorial = function() {
-	
-};
+console.log(sum(7, 11))
 
 // Do not edit below this line
 module.exports = {
