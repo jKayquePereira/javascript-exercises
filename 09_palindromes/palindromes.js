94f9ab8d1a5863bf1d163735444382933efb09c2
Kayque Pereira <114672278+jKayquePereira@users.noreply.github.com>
2024-11-27 22:20:10 -0300

    exc9 finalizado

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let newWord = word.replace(/[\W_]/g, '').toLowerCase().split(',').join('')
 
+    return newWord === newWord.split('').reverse().join('');
 };
 
 // Do not edit below this line
