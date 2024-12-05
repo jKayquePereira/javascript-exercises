59b796e1df387a2da124382d27aa8866b70c138d
Kayque Pereira <114672278+jKayquePereira@users.noreply.github.com>
2024-12-04 23:09:46 -0300

    exc8 finalizado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(numAdd1, numAdd2) {
+  return numAdd1 + numAdd2
 };
 
-const subtract = function() {
-	
+const subtract = function(numSub1, numSub2) {
+	return numSub1 - numSub2
 };
 
-const sum = function() {
-	
+const sum = function(numSum) {
+  if (numSum.length === 0) {
+    return 0
+  }
+
+  return numSum.reduce((total, item) => total + item)
 };
 
-const multiply = function() {
+const multiply = function(numMult) {
+  if (numMult.length === 0) {
+    return 0
+  }
 
+  return numMult.reduce((total, item) => total * item)
 };
 
-const power = function() {
-	
+const power = function(numPower1, numPower2) {
+  return numPower1 ** numPower2
 };
 
-const factorial = function() {
-	
+const factorial = function(numFac) {
+  if (numFac === 0 || numFac === 1) {
+    return 1
+  }
+
+  for (i = numFac - 1; i >= 1; i--) {
+    numFac *= i
+  }
+  return numFac
 };
 
 // Do not edit below this line
